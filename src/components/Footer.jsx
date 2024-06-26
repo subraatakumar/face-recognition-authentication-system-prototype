a545d6717bbcace98c3c2f20b24663d3fe30abe3
subraatakumar <business.ttrc@gmail.com>
2024-06-26 08:59:30 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <h6 className="text-sm text-center w-full">
+      Created with 💙 by{" "}
+      <a
+        href="https://github.com/subraatakumar"
+        target="_blank"
+        alt="Subrata's GitHub Account"
+        className="hover:underline"
+      >
+        Subrata
+      </a>
+    </h6>
+  );
+}
+
+export default Footer;
